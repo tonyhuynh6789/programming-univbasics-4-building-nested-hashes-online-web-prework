a456bc4d229f491838bec64b3b8c475ba411795d
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-26 22:52:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 def monopoly_with_third_tier
 {
    :railroads => {
-     :rent_in_dollars => {:one_piece_owned => "25", }
+     :rent_in_dollars => {:one_piece_owned => "25",:two_pieces_owned => "50" }
     
    }
   }
