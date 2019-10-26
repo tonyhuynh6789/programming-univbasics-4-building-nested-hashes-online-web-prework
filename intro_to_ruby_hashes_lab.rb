e00a7d4c9caad00c2c67a1be577ebf3cb96571c1
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-26 22:59:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
      :reading_railroad => {},
      :pennsylvania_railroad => {},
      :b_and_o_railroad => {},
-     
+     :shortline_railroad => {}
    }
   }
 end
