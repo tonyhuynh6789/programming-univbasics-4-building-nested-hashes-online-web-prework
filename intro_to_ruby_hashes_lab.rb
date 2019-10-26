a7fe22de799b9855b810a566276f1f7c399a807f
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-26 23:12:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_fourth_tier
    :railroads => {
      :rent_in_dollars => {:one_piece_owned => 25,:two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200 },
    :names => {
-     :reading_railroad => {mortgage: "100"},
+     :reading_railroad => {mortgage: "$100"},
      :pennsylvania_railroad => {},
      :b_and_o_railroad => {},
      :shortline_railroad => {}
